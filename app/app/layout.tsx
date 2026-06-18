bb8ed8faae584b699fd196f56cd904136248dad7
BAMToolz <jballtwo@gmail.com>
2026-06-18 14:04:55 -0400

    Create layout.tsx

---
+++ app/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "BAMToolz",
+  description: "Ball Advanced Maintenance Tools"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
